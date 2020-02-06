110447b03b72ab21109258f83099f876dea66d9a
Kribon <60216150+Kribon@users.noreply.github.com>
2020-02-06 10:46:20 +0530

    Update and rename sort.cpp to sort.h

--- week-2/sort.h
+++ week-2/sort.h
@@ -1,9 +1,4 @@
-#include <iostream>
-#include <vector>
 
-#include "check.h"
-
-using namespace std;
 
 void merge(long arr[], int l, int m, int r) 
 { 
@@ -66,29 +61,4 @@ void mergeSort(long arr[], int l, int r)
     } 
 } 
 
-int main()
-{
-  int n;
-  cin >>n; //take number of elements in array
-  long arr[n];
-  vector<int> to_sort(n);
-  for(int i=0;i<n;i++)
-  {
-    cin>>arr[i];
-    cin>>to_sort(i);
-    }
-   mergeSort(arr,0,n-1);
-  // check for corectness of algorithm
-  sort(to_sort.begin(),to_sort.end());
-  if(check(to_sort,arr) 
-     {
-         cout<<"Your Algorithm is correct"<<endl;
-         for(int i=0;i<n;i++) 
-           {
-             cout<<arr[i]<<' ';
-           }
-     }
-     else 
-       cout<<"Please check your Algorithm";
-  return 0;
- }
+
