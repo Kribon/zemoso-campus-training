c6cbb25234251e8794fc27b5d93552f444a4e514
Kribon <60216150+Kribon@users.noreply.github.com>
2020-02-06 10:51:06 +0530

    Update main.cpp

--- week-2/main.cpp
+++ week-2/main.cpp
@@ -11,16 +11,16 @@ int main()
   int n;
   cin >>n; //take number of elements in array
   long arr[n];
-  vector<int> to_sort(n);
+  vector<int> vec(n);
   for(int i=0;i<n;i++)
   {
     cin>>arr[i];
-    cin>>to_sort(i);
+    cin>>vec[i];
     }
    mergeSort(arr,0,n-1);
   // check for corectness of algorithm
-  sort(to_sort.begin(),to_sort.end());
-  if(check(to_sort,arr) 
+  sort(vec.begin(),vec.end());
+  if(check(vec,arr)) 
      {
          cout<<"Your Algorithm is correct"<<endl;
          for(int i=0;i<n;i++) 
