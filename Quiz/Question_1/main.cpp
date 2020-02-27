4ae10018902c35231a38642b30f0693511625e72
Kribon <60216150+Kribon@users.noreply.github.com>
2020-02-27 15:43:20 +0530

    Update main.cpp

--- Quiz/Question_1/main.cpp
+++ Quiz/Question_1/main.cpp
@@ -8,10 +8,13 @@ using namespace std;
 int main(int argc, char *argv[]) 
 { 
   
-    unsigned long n = stoi(argv[1]);
+     long n = stoi(argv[1]);
+  if(n<0) cout<<"Enter a positive number";
+  else if(n==0) cout<<"Zero";
+  else{
     string towords=convert(n);
     cout << towords << endl; 
-   
+  }
   
     return 0; 
 } 
