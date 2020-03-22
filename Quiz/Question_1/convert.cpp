717d2f23500df15f1e9738b6cddf056c9a9073ea
Kribon <60216150+Kribon@users.noreply.github.com>
2020-03-22 09:30:39 +0530

    Update convert.cpp

--- Quiz/Question_1/convert.cpp
+++ Quiz/Question_1/convert.cpp
@@ -24,16 +24,16 @@ string convert long n){
         return tens[n/10] + ((n%10!=0)?" ":"") + convert(n%10);
     }
     if(n<1000) {
-            return ones[n / 100] + " hundred" + ((n % 100 != 0) ? " " : "") + convert(n % 100);
+            return ones[n / 100] + " hundred and" + ((n % 100 != 0) ? " " : "") + convert(n % 100);
         }
     if (n < 1000000) {
-            return convert(n / 1000) + " thousand" + ((n % 1000 != 0) ? " " : "") + convert(n % 1000);
+            return convert(n / 1000) + " thousand and" + ((n % 1000 != 0) ? " " : "") + convert(n % 1000);
         }
     if (n < 1000000000) {
-            return convert(n / 1000000) + " million" + ((n % 1000000 != 0) ? " " : "") + convert(n % 1000000);
+            return convert(n / 1000000) + " million and" + ((n % 1000000 != 0) ? " " : "") + convert(n % 1000000);
         }
     if(n<1000000000000) {
-            return convert(n / 1000000000) + " billion"  + ((n % 1000000000 != 0) ? " " : "") + convert(n % 1000000000);
+            return convert(n / 1000000000) + " billion and"  + ((n % 1000000000 != 0) ? " " : "") + convert(n % 1000000000);
     }
-        return convert(n/1000000000000) + " trillion" + ((n%1000000000000 != 0)? " ": "")+ convert(n%1000000000000);
+        return convert(n/1000000000000) + " trillion and" + ((n%1000000000000 != 0)? " ": "")+ convert(n%1000000000000);
  }
